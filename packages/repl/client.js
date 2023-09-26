b657bdb6fc794abc46f4b37086aff0d93ca6203b
azizzaeny <zie.azlance@gmail.com>
2023-09-26 02:50:59 +0000

    remove not needed files

--- packages/repl/client.js
+++
@@ -1,9 +0,0 @@
-// long subscribe polling connect to repl
-export function start_repl(origin, opts){
-  origin = origin  || window.location.origin;
-  opts = opts || {'repl': true };
-  return fetch(origin, opts).then(res => res.text())
-    .then(res => (console.log('evaluate:'), res))
-    .then(res => eval(res))
-    .then(_ => setTimeout(() => start_repl(origin, opts), 100))
-}      
