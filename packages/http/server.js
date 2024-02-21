9f7eeac1439ad5203e5a0b92ff90ee2889680507
azizzaeny <zie.azlance@gmail.com>
2024-02-21 04:17:30 +0000

    update http server

--- packages/http/server.js
+++ packages/http/server.js
@@ -24,18 +24,36 @@ function createServer(changeHandler, port=8080){
   server.listen(port, log('start', `listening at port ${port}`));
 }
 
+function parseUrl(request){  
+  (request.parsed = url.parse(request.url, true));
+  (request.query = request.parsed.query);
+  (request.path = request.parsed.pathname);
+  return true;
+}
+
+function parseBody(request, data){
+  (request.body = Buffer.concat(data).toString());
+  if(request.headers && request.headers['content-type'] === "application/json"){
+    request.body = JSON.parse(request.body);
+  };
+  return true;
+}
+
+function matchRoutes(routes, request){
+  let match = routes.find((route)=>(route.method === request.method && route.path === request.path)) || {action: "notFound"};
+  return match;
+}
+
 function requestHandler(changeHandler){
   return function(request, response){
     let data = [];
     request
       .on('data', (chunk)=> { if(request.method !== "GET" && chunk) (data.push(chunk))})
       .on('end', async () => {
-        (request.parsed = url.parse(request.url, true));
-        (request.body = Buffer.concat(data).toString());
-        (request.query = request.parsed.query);
-        (request.path = request.parsed.pathname);
-        let {routes, actions} = changeHandler(request, response);
-        let match = routes.find((route)=>(route.method === request.method && route.path === request.path)) || {action: "notFound"};
+        parseUrl(request);
+        parseBody(request, data);
+        let {routes, actions} = changeHandler(request);
+        let match = matchRoutes(routes, request);
         let command = actions[match['action']];
         var object = {status: 404, headers:{}, body: 'not-found'};        
         if(match && command) (object = await command(request, response));  
