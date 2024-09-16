98a7d0524cd43ca17e2a5ce49de9136a6618d523
azizzaeny <zie.azlance@gmail.com>
2024-09-17 02:15:43 +0700

    complete build with write out files to cjs and mjs

--- packages/literate/build.js
+++ packages/literate/build.js
@@ -13,17 +13,24 @@ var writeSync = (file, content, utf8 = 'utf8') => fs.writeFileSync(file, content
 
 var build = (path='./') =>{
   let blocks = readDir(path).filter(onlyMarkdown).map(captureCode).flat().filter(f => f.context);
-  // todo: create module format output
-  // - module import
-  // - core body
-  // - node.js 
-  // - export 
-  // todo: create commonjs format output
-  // - require module
-  // - core body
-  // - node.js
-  // - module.exports
-  return blocks;
+  let context = (ctx) => (b) => b.lang === 'js' && b.context === ctx;
+  let moduleImport = blocks.find(b => context('deps')(b) && b.type === 'module').content;
+  let commonJsImport = blocks.find(b => context('deps')(b) && b.type === 'commonjs').content;
+  let core = blocks.find(context('core')).content;
+  let node = blocks.find(context('node')).content;
+  let nodeExport = blocks.find(b => context('export')(b) && b.type==='node').content;
+  let coreExport = blocks.find(b => context('export')(b) && b.type==='core').content;
+  let indexExport = blocks.find(b => context('export')(b) && b.type==='index').content;  
+  let files = {
+    './src/cjs/core.js': `${core}\n\nmodule.exports=${coreExport}`,
+    './src/mjs/core.mjs': `${core}\n\nexport ${coreExport}`,
+    './src/cjs/node.js': `${commonJsImport}\n${node}\n\nmodule.exports=${nodeExport}`,
+    './src/mjs/node.mjs': `${moduleImport}\n${node}\n\nexport ${nodeExport}`,
+    './src/cjs/index.js': `var ${coreExport} = require('./core');\nvar ${nodeExport} = require('./node');\n\nmodule.exports = ${indexExport}`,
+    './src/mjs/index.mjs': `import ${coreExport} from "./core.mjs";\nimport ${nodeExport} from "./node.mjs";\n\nexport ${indexExport}`,
+  }; 
+  let out = Object.entries(files).map(([loc, contents]) => (fs.writeFileSync(loc, contents, 'utf8'), loc));
+  return out;
 }
 
 console.log(build());
